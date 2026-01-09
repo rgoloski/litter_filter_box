7157c07432a90715e5e5b9703102a1ecbf93e55b
Ricky Goloski <rgoloski@gmail.com>
2026-01-08 22:58:26 -0500

    Button cycles duty cycle

--- Core/Src/fan_control.c
+++ Core/Src/fan_control.c
@@ -16,20 +16,28 @@ extern TIM_HandleTypeDef htim1;
 
 static bool initialized = false;
 
+static uint32_t duty = 10;
+
 void run_duty_control(void)
 {
 	if (!initialized)
 	{
-	    MX_TIM1_SetDuty(25);
 	    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
 	    initialized = true;
 	}
+
+	MX_TIM1_SetDuty(duty);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	if (GPIO_Pin == B1_Pin)
 	{
-		// Handle button here
+        // Increment by 5 but limit to 50% for now
+		duty += 5;
+		if (duty > 50)
+		{
+			duty = 10;
+		}
 	}
 }
